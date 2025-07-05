3a804caebc8660ba9af74fa028fbc1d4b9e08cc6
Sagar Bisht <sagarbisht409@gmail.com>
2025-07-05 17:55:30 +0530

    commited

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGO_DB_URL as string;
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGO_DB_URL environment variable");
